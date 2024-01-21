109536df973b0d18b54458a90d783f423bbacbfa
selectdev <selectdev01@gmail.com>
2024-01-21 13:48:05 +0000

    Discord Bot. Yes.

---
+++ src/commands/uptime.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export default {
+	data: {
+		meta: new SlashCommandBuilder()
+			.setName("uptime")
+			.setDescription("How long have i been alive for?"),
+		permissionRequired: null,
+	},
+	async execute(client, interaction) {
+		const formatTime = (seconds) => {
+			const days = Math.floor(seconds / 86400);
+			seconds -= days * 86400;
+
+			const hours = Math.floor(seconds / (60 * 60));
+			seconds -= hours * 3600;
+
+			const minutes = Math.floor((seconds % (60 * 60)) / 60);
+			seconds -= minutes * 60;
+
+			const secs = Math.floor(seconds % 60);
+
+			return `${days} days, ${hours} hours, ${minutes} minutes, ${secs} seconds`;
+		};
+
+		await interaction.reply({
+			content: formatTime(process.uptime()),
+		});
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -2,111 +2,35 @@
 import fs from "fs";
 import path from "path";
 import * as database from "./v4-database/prisma.js";
-import * as perms from "./perms.js";
-import cors from "@fastify/cors";
-import ratelimit from "@fastify/rate-limit";
-import swagger from "@fastify/swagger";
-import ui from "@fastify/swagger-ui";
+import { hasPerm } from "./perms.js";
+import {
+	Client,
+	GatewayIntentBits,
+	Events,
+	ActivityType,
+	SlashCommandBuilder,
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	codeBlock,
+	ModalSubmitInteraction,
+} from "discord.js";
+import { debug, info, error } from "./logger.js";
 import "dotenv/config";
-import Fastify, { FastifyInstance } from "fastify";
 
-// Middleware
-const app: FastifyInstance = Fastify({
-	logger: true,
-});
+// Config
+let DISCORD_SERVER_URI: String = "https://discord.gg/XdGs8WFFtK";
 
-app.register(cors, {
-	origin: "*",
-	allowedHeaders: [
-		"secret",
-		"userid",
-		"Authorization",
-		"Authorization",
-		"Content-Type",
-		"Content-Disposition",
-		"Content-Length",
+// Create Discord Client
+const client: Client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildPresences,
 	],
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-	credentials: true,
-	optionsSuccessStatus: 200,
-	preflight: true,
-	strictPreflight: false,
 });
 
-app.register(swagger, {
-	swagger: {
-		info: {
-			title: "Select List",
-			description:
-				"Enhance your Discord Server with unparalleled versatility through our curated selection.",
-			version: "4.0.0",
-		},
-		host:
-			process.env.ENV === "production"
-				? "api.select-list.xyz"
-				: `localhost:${process.env.PORT}`,
-		schemes: ["http"],
-		consumes: ["application/json"],
-		produces: ["application/json"],
-		tags: [
-			{
-				name: "users",
-				description: "Endpoints related to our users database.",
-			},
-			{
-				name: "bots",
-				description: "Endpoints related to our bots database.",
-			}
-		],
-		securityDefinitions: {
-			apiKey: {
-				type: "apiKey",
-				name: "Authorization",
-				in: "header",
-			},
-		},
-	},
-	hideUntagged: false,
-});
-
-app.register(ui, {
-	routePrefix: "/docs",
-	uiConfig: {
-		docExpansion: "full",
-		deepLinking: true,
-	},
-	uiHooks: {
-		onRequest: (request, reply, next) => {
-			next();
-		},
-		preHandler: (request, reply, next) => {
-			next();
-		},
-	},
-	staticCSP: true,
-	transformStaticCSP: (header) => header,
-	transformSpecification: (swaggerObject, request, reply) => {
-		return swaggerObject;
-	},
-	transformSpecificationClone: true,
-});
-
-app.register(ratelimit, {
-	global: true,
-	max: 50,
-	timeWindow: 1000,
-});
-
-app.addHook("preHandler", (req, res, done) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "*");
-	res.header("Access-Control-Allow-Methods", "*");
-	res.header("Access-Control-Allow-Credentials", "true");
-
-	done();
-});
-
-// API Endpoints Map
+// Get files from directory
 const getFilesInDirectory = (dir: string) => {
 	let files: string[] = [];
 	const filesInDir = fs.readdirSync(dir);
@@ -123,29 +47,206 @@ const getFilesInDirectory = (dir: string) => {
 	return files;
 };
 
-// API Endpoints
-const apiEndpointsFiles = getFilesInDirectory("./dist/endpoints").filter(
-	(file) => file.endsWith(".js")
+// Add Commands
+const commands: Map<
+	string,
+	{
+		data: {
+			meta: SlashCommandBuilder;
+			permissionRequired: string | null;
+		};
+		execute: (
+			client: Client,
+			interaction: ChatInputCommandInteraction
+		) => Promise<void>;
+	}
+> = new Map();
+const commandFiles = getFilesInDirectory("./dist/commands").filter((file) =>
+	file.endsWith(".js")
 );
 
-for (const file of apiEndpointsFiles) {
+for (const file of commandFiles) {
 	import(`../${file}`)
-		.then(async (module) => {
-			await app.route(module.default);
+		.then((module) => {
+			const i = module.default;
+			commands.set(i.data.meta.name, i);
 		})
 		.catch((error) => {
 			console.error(`Error importing ${file}: ${error}`);
 		});
 }
 
-setTimeout(() => {
-	// Swagger
-	app.ready(() => {
-		app.swagger();
-	});
+// Add Modals
+const modals: Map<
+	string,
+	{
+		data: {
+			name: string;
+			permissionRequired: string | null;
+		};
+		execute: (
+			client: Client,
+			interaction: ModalSubmitInteraction
+		) => Promise<void>;
+	}
+> = new Map();
+const modalFiles = getFilesInDirectory("./dist/modals").filter((file) =>
+	file.endsWith(".js")
+);
 
-	// Start Server
-	app.listen({ port: Number(process.env.PORT) }, (err) => {
-		if (err) throw err;
-	});
-}, 5000);
+for (const file of modalFiles) {
+	import(`../${file}`)
+		.then((module) => {
+			const i = module.default;
+			modals.set(i.data.name, i);
+		})
+		.catch((error) => {
+			console.error(`Error importing ${file}: ${error}`);
+		});
+}
+
+// Debug Event
+client.on("debug", (info) => {
+	debug("Discord", info);
+});
+
+// Error Event
+client.on("error", (p) => {
+	error("Discord", p.toString());
+});
+
+// Ready Event
+client.on(Events.ClientReady, () => {
+	info("Discord", `Logged in as ${client.user?.tag}!`);
+	client.user?.setStatus("dnd");
+	client.user?.setActivity("SL V4", { type: ActivityType.Watching });
+});
+
+// Interaction Event
+client.on(Events.InteractionCreate, async (interaction) => {
+	// Slash Command
+	if (interaction.isChatInputCommand()) {
+		const command = commands.get(interaction.commandName);
+		if (!command) return;
+
+		try {
+			if (command.data.permissionRequired) {
+				const user = await database.Users.get({
+					userid: interaction.user.id,
+				});
+
+				if (user) {
+					if (
+						hasPerm(
+							user.staff_perms,
+							command.data.permissionRequired
+						)
+					)
+						await command?.execute(client, interaction);
+					else
+						await interaction.reply({
+							embeds: [
+								new EmbedBuilder()
+									.setTitle("Oops! Missing Permissions!")
+									.setDescription(
+										`You do not have enough permissions to execute this command.\nPermissions Provided: **${user.staff_perms.join(", ") || "None"}**\n Permission Required: **${command.data.permissionRequired}**.`
+									)
+									.setColor("Random"),
+							],
+						});
+				} else
+					await interaction.reply({
+						embeds: [
+							new EmbedBuilder()
+								.setTitle("Oops! Missing Permissions!")
+								.setDescription(
+									`You do not have enough permissions to execute this command. Permission Required: **${command.data.permissionRequired}**.`
+								)
+								.setColor("Random"),
+						],
+					});
+			} else await command?.execute(client, interaction);
+		} catch (p) {
+			error("Discord", p.toString());
+
+			await interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setTitle("Oops! We had an issue.")
+						.setDescription(
+							`This issue has been reported to our developers. If you continue to having issues with our bot, you may join our [Discord Server](${DISCORD_SERVER_URI})`
+						)
+						.setColor("Random")
+						.addFields({
+							name: "Error",
+							value: codeBlock("javascript", p),
+							inline: false,
+						}),
+				],
+			});
+		}
+	}
+
+	// Modal
+	if (interaction.isModalSubmit()) {
+		const modal = modals.get(interaction.customId);
+		if (!modal) return;
+
+		try {
+			if (modal.data.permissionRequired) {
+				const user = await database.Users.get({
+					userid: interaction.user.id,
+				});
+
+				if (user) {
+					if (
+						hasPerm(user.staff_perms, modal.data.permissionRequired)
+					)
+						await modal?.execute(client, interaction);
+					else
+						await interaction.reply({
+							embeds: [
+								new EmbedBuilder()
+									.setTitle("Oops! Missing Permissions!")
+									.setDescription(
+										`You do not have enough permissions to execute this command.\nPermissions Provided: **${user.staff_perms.join(", ") || "None"}**\n Permission Required: **${modal.data.permissionRequired}**.`
+									)
+									.setColor("Random"),
+							],
+						});
+				} else
+					await interaction.reply({
+						embeds: [
+							new EmbedBuilder()
+								.setTitle("Oops! Missing Permissions!")
+								.setDescription(
+									`You do not have enough permissions to execute this command. Permission Required: **${modal.data.permissionRequired}**.`
+								)
+								.setColor("Random"),
+						],
+					});
+			} else await modal?.execute(client, interaction);
+		} catch (p) {
+			error("Discord", p.toString());
+
+			await interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setTitle("Oops! We had an issue.")
+						.setDescription(
+							`This issue has been reported to our developers. If you continue to having issues with our bot, you may join our [Discord Server](${DISCORD_SERVER_URI})`
+						)
+						.setColor("Random")
+						.addFields({
+							name: "Error",
+							value: codeBlock("javascript", p),
+							inline: false,
+						}),
+				],
+			});
+		}
+	}
+});
+
+// Login to Discord
+client.login(process.env.DISCORD_TOKEN);
